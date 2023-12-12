b59ebb3ba3e52a8dd1e15f3191958cb569f58d67
catxcoding <145952390+catxcoding@users.noreply.github.com>
2023-12-12 14:58:05 -0800

    Added to index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+// index.js
+const getUserInput = require('./lib/userInput');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+async function runLogoMaker() {
+  // Get user input
+  const userInput = await getUserInput();
+
+  // Create the corresponding shape based on user choice
+  let shape;
+  switch (userInput.shape) {
+    case 'circle':
+      shape = new Circle();
+      break;
+    case 'triangle':
+      shape = new Triangle();
+      break;
+    case 'square':
+      shape = new Square();
+      break;
+    default:
+      console.error('Invalid shape choice');
+      return;
+  }
+
+  // Set colors
+  shape.setColor(userInput.shapeColor);
+
+
+  // Generate the SVG string
+  const svgString = shape.render();
+
+  console.log(svgString);
+}
+
+// Call the function to start the application
+runLogoMaker();
